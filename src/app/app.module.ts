911c493ac6119e7136a9fce86b0082e2a5949b27
baligh8 <obaligh8@gmail.com>
2023-06-14 00:02:47 +0100

    connection CLAIM FEEDBACK UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { TempComponent } from './temp/temp.component';
+import { ContactComponent } from './contact/contact.component';
+import { SingupComponent } from './singup/singup.component';
+import { AddAnnonceComponent } from './add-annonce/add-annonce.component';
+import { ListAnnoncesComponent } from './list-annonces/list-annonces.component';
+import { DetailAnnonceComponent } from './detail-annonce/detail-annonce.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AccueilComponent,
+    TempComponent,
+    ContactComponent,
+    SingupComponent,
+    AddAnnonceComponent,
+    ListAnnoncesComponent,
+    DetailAnnonceComponent
   ],
   imports: [
     BrowserModule,
